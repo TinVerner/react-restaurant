70f696f0bb3e8af97d7306e10066a4033a0ebe20
TinVerner <tinverner@gmail.com>
2024-06-21 16:10:31 +0300

    21.06.24 menu and about example complteted

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,65 @@
 import React from 'react'
+import './About.css'
+import AboutChef1 from '../utils/img/chef1.jpg'
+import AboutChef2 from '../utils/img/chef2.jpg'
+import { ImageGallery } from '../components/ImageGallery'
 
 function About() {
 	return (
-		<div>
-			<h1 className='mt-5'>О нас</h1>
+		<div className='about-page'>
+			<header className='mt-5'>
+				<div className='container h-100 d-flex align-items-center justify-content-center'>
+					<h1 className='text-light'>О нас</h1>
+				</div>
+			</header>
+
+			<div className='container my-5'>
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum quasi
+					illo est quaerat ab cumque provident illum laudantium laboriosam aut
+					necessitatibus, excepturi quidem cum amet rerum in odio ratione natus
+					doloremque exercitationem repellat fugit earum eaque. Minus, saepe.
+					Tenetur laborum quis dolores blanditiis eos possimus earum facere,
+					delectus nostrum quaerat?
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ratione
+					consequuntur, officiis id alias qui atque? Distinctio rem possimus
+					ullam dolorum tempore. In magni eius laborum incidunt aliquam nemo
+					autem a molestias sed, iure explicabo tempora amet delectus? Magnam
+					architecto tempore id explicabo excepturi voluptas sed quasi
+					aspernatur quos magni. Architecto ipsam ullam voluptas maxime unde
+					repellendus rem accusamus porro nostrum, iusto itaque laboriosam
+					inventore animi ad, alias sequi? Aut, nam aperiam. Enim libero error
+					explicabo voluptatibus! Accusamus, consectetur laborum.
+				</p>
+
+				<div className='row'>
+					<div className='col-lg-6'>
+						<img src={AboutChef1} className='img-fluid my-4' alt='' />
+					</div>
+					<div className='col-lg-6'>
+						<img src={AboutChef2} className='img-fluid my-4' alt='' />
+					</div>
+				</div>
+
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+					sint hic obcaecati quo distinctio qui quod exercitationem maiores sunt
+					aspernatur sed aut, praesentium ipsa a aperiam repellendus asperiores
+					eos assumenda quas dolore, fugit expedita harum totam quia? Libero
+					modi ipsum quasi cum incidunt numquam, sequi dolore ratione quos sunt.
+					A doloribus iste dignissimos qui maiores blanditiis est iusto?
+					Expedita odio obcaecati porro pariatur ea totam maiores culpa hic
+					fugit doloremque quam debitis labore natus laborum accusamus id nihil
+					facilis voluptatem eius, aliquam animi! Quaerat, quam libero eveniet
+					error quisquam quod.
+				</p>
+			</div>
+
+			<div className='bg-dark text-light'>
+				<ImageGallery />
+			</div>
 		</div>
 	)
 }
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -27,6 +27,69 @@ const breakfast = [
 	},
 ]
 
+const lunch = [
+	{
+		id: 1,
+		name: 'Салат',
+		desc: 'smoked bacon, sausage, tomato, mushrooms, black pudding, baked beans, eggs',
+		price: '20 BYN',
+	},
+	{
+		id: 2,
+		name: 'Спагетти',
+		desc: 'avocado, poached egg, onion, tomatoes, bread',
+		price: '10 BYN',
+	},
+	{
+		id: 3,
+		name: 'Роллы',
+		desc: 'tortilla, egg, cheese, potatoes, pork meat',
+		price: '15 BYN',
+	},
+]
+
+const dinner = [
+	{
+		id: 1,
+		name: 'Beef',
+		desc: 'smoked bacon, sausage, tomato, mushrooms, black pudding, baked beans, eggs',
+		price: '20 BYN',
+	},
+	{
+		id: 2,
+		name: 'спагетти болонезе',
+		desc: 'avocado, poached egg, onion, tomatoes, bread',
+		price: '10 BYN',
+	},
+	{
+		id: 3,
+		name: 'Карри',
+		desc: 'tortilla, egg, cheese, potatoes, pork meat',
+		price: '15 BYN',
+	},
+]
+
+const dessert = [
+	{
+		id: 1,
+		name: 'Десерт 1',
+		desc: 'smoked bacon, sausage, tomato, mushrooms, black pudding, baked beans, eggs',
+		price: '20 BYN',
+	},
+	{
+		id: 2,
+		name: 'Десерт 2',
+		desc: 'avocado, poached egg, onion, tomatoes, bread',
+		price: '10 BYN',
+	},
+	{
+		id: 3,
+		name: 'Десерт 3',
+		desc: 'tortilla, egg, cheese, potatoes, pork meat',
+		price: '15 BYN',
+	},
+]
+
 function Menu() {
 	return (
 		<div className='menu-page'>
@@ -45,6 +108,133 @@ function Menu() {
 						<div className='col-lg-6 d-flex justify-content-center'>
 							<img
 								src={BreakfastImg}
+								className='img-fluid w-75 mt-4 mt-lg-0'
+								alt=''
+							/>
+						</div>
+						<div className='col-lg-6 d-flex flex-column justify-content-around'>
+							{breakfast.map(breakfast => (
+								<div key={breakfast.id}>
+									<Card className='border-0'>
+										<CardBody>
+											<CardTitle className='text-center fs-3'>
+												{breakfast.name}
+											</CardTitle>
+											<CardText className='text-center fs-5'>
+												{breakfast.desc}
+											</CardText>
+											<CardText className='text-center fs-3 text-success'>
+												{breakfast.price}
+											</CardText>
+										</CardBody>
+									</Card>
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className='lunch py-5 bg-dark'>
+				<div className='container'>
+					<h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>
+						Lunch
+					</h2>
+					<div className='row'>
+						<div className='col-lg-6 d-flex flex-column justify-content-around'>
+							{lunch.map(lunch => (
+								<div key={lunch.id}>
+									<Card className='text-light border-0 bg-dark'>
+										<CardBody>
+											<CardTitle className='text-center fs-3'>
+												{lunch.name}
+											</CardTitle>
+											<CardText className='text-center fs-5'>
+												{lunch.desc}
+											</CardText>
+											<CardText className='text-center fs-3 fw-bold text-success'>
+												{lunch.price}
+											</CardText>
+										</CardBody>
+									</Card>
+								</div>
+							))}
+						</div>
+						<div className='col-lg-6 d-flex justify-content-center'>
+							<img
+								src={LunchImg}
+								className='img-fluid w-75 mt-4 mt-lg-0'
+								alt=''
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className='dinner my-5'>
+				<div className='container'>
+					<h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>
+						Dinner
+					</h2>
+					<div className='row flex-column-reverse flex-lg-row'>
+						<div className='col-lg-6 d-flex justify-content-center'>
+							<img
+								src={DinnerImg}
+								className='img-fluid w-75 mt-4 mt-lg-0'
+								alt=''
+							/>
+						</div>
+						<div className='col-lg-6 d-flex flex-column justify-content-around'>
+							{dinner.map(dinner => (
+								<div key={dinner.id}>
+									<Card className='border-0'>
+										<CardBody>
+											<CardTitle className='text-center fs-3'>
+												{dinner.name}
+											</CardTitle>
+											<CardText className='text-center fs-5'>
+												{dinner.desc}
+											</CardText>
+											<CardText className='text-center fs-3 text-success'>
+												{dinner.price}
+											</CardText>
+										</CardBody>
+									</Card>
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className='dessert py-5 bg-dark'>
+				<div className='container'>
+					<h2 className='text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-success'>
+						Dessert
+					</h2>
+					<div className='row'>
+						<div className='col-lg-6 d-flex flex-column justify-content-around'>
+							{dessert.map(dessert => (
+								<div key={dessert.id}>
+									<Card className='text-light border-0 bg-dark'>
+										<CardBody>
+											<CardTitle className='text-center fs-3'>
+												{dessert.name}
+											</CardTitle>
+											<CardText className='text-center fs-5'>
+												{dessert.desc}
+											</CardText>
+											<CardText className='text-center fs-3 fw-bold text-success'>
+												{dessert.price}
+											</CardText>
+										</CardBody>
+									</Card>
+								</div>
+							))}
+						</div>
+						<div className='col-lg-6 d-flex justify-content-center'>
+							<img
+								src={DessertImg}
 								className='img-fluid w-75 mt-4 mt-lg-0'
 								alt=''
 							/>
